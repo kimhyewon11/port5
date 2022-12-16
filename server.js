aa34bc421ee9cc4f9334a20b2f69b33ca7999693
kim <haewon_11@naver.com>
2022-12-16 10:46:47 +0900

    포폴 3 작업 완료

--- server.js
+++ server.js
@@ -38,47 +38,135 @@ MongoClient.connect("mongodb+srv://admin:qwer1234@testdb.mopkvcj.mongodb.net/?re
 
 //메인페이지
 app.get("/",(req,res)=>{
-    res.render("index",{userdata:req.user})
+    db.collection("prdlist").find({}).toArray((err,result)=>{
+        res.render("index",{prdData:result,userdata:req.user});
+    })
 });
 
-//prd 페이지
-app.get("/shop",(req,res)=>{
-    res.render("shop",{userdata:req.user})
-});
-//prd 상세 페이지
-app.get("/shopdetail",(req,res)=>{
-    res.render("shopdetail",{userdata:req.user})
-});
+
+
 
 //news 페이지
 app.get("/news",(req,res)=>{
-    res.render("news",{userdata:req.user})
+    db.collection("newslist").find({}).toArray((err,result)=>{
+        res.render("news",{newsData:result,userdata:req.user})
+    })
 });
 
 // news 상세 페이지
-app.get("/newsdetail",(req,res)=>{
-    res.render("newsdetail",{userdata:req.user})
+app.get("/newsdetail/:no",(req,res)=>{
+    db.collection("newslist").findOne({num:Number(req.params.no)},(err,result)=>{
+        res.render("newsdetail",{newsData:result,userdata:req.user})
+    })
 });
 
+
 //공지 페이지
-app.get("/notice",(req,res)=>{
-    res.render("notice",{userdata:req.user})
+app.get("/notice",async(req,res)=>{
+
+
+     //사용자가 게시판에 접속시 몇번 페이징 번호로 접속했는지 체크 
+    let pageNumber = (req.query.page == null) ? 1 : Number(req.query.page) ;
+    //한 페이지당 보여줄 데이터 갯수 
+    let perPage = 5;
+    //블록당 보여줄 페이지 번호 갯수
+    let blockCount = 5;
+    //현재 페이지 블록 구하기 
+    let blockNum =Math.ceil(pageNumber / blockCount) ;
+    //블록 안에 있는 페이징의 시작번호 값 알아내기
+    let blockStart = ((blockNum - 1) * blockCount ) + 1 ;
+    //블록 안에 있는 페이징의 끝번호 값 알아내기
+    let blockEnd = blockStart + blockCount - 1 ;
+
+    
+
+    //데이터베이스 콜렉션에 있는 전체 객체의 갯수값 가져오는 명령어 
+    let totalData = await db.collection("noticelist").countDocuments({});
+    //전체 데이터 값을 통해서 ---> 몇개의 페이징 번호가 만들어져야 하는지 계산
+    let paging =  Math.ceil(totalData / perPage) ;
+    //블록에서 마지막 번호가 페이징의 끝번호보다 크ㅏ면 페이징의 끝번호를 강제로 부여
+    if(blockEnd > paging){
+        blockEnd = paging;
+    }
+    //블록의 총 갯수 구하기
+    let totalBlock = Math.ceil(paging / blockCount);
+    //데이터베이스에서 꺼내오는 데이터의 시작 순번값을 결정 
+    let startFrom = (pageNumber -1 ) * perPage;
+
+
+    console.log(totalData)
+
+    db.collection("noticelist").find({}).sort({num:-1}).skip(startFrom).limit(perPage).toArray((err,result)=>{
+        res.render("notice",{
+            noticeData:result,
+            userdata:req.user,
+            paging:paging,
+            pageNumber:pageNumber,
+            blockStart:blockStart,
+            blockEnd:blockEnd,
+            blockNum:blockNum,
+            totalBlock:totalBlock
+        })
+
+    })
 });
 
 //공지 디테일 페이지
-app.get("/noticedt",(req,res)=>{
-    res.render("noticedetail",{userdata:req.user})
+app.get("/noticedt/:no",(req,res)=>{
+    db.collection("noticelist").findOne({num:Number(req.params.no)},(err,result)=>{
+        res.render("noticedetail",{noticeData:result,userdata:req.user})
+    })
 });
 
 //매장 페이지
 app.get("/store",(req,res)=>{
-    res.render("store",{userdata:req.user})
+    db.collection("storelist").find({}).toArray((err,result)=>{
+        res.render("store",{storeData:result,userdata:req.user})
+    })
 });
 
 //게시판 페이지
-app.get("/board",(req,res)=>{
-    db.collection("board").find({}).toArray((err,result)=>{
-        res.render("brdlist",{userdata:req.user,brddata:result})
+app.get("/board",async(req,res)=>{
+      //사용자가 게시판에 접속시 몇번 페이징 번호로 접속했는지 체크 
+      let pageNumber = (req.query.page == null) ? 1 : Number(req.query.page) ;
+      //한 페이지당 보여줄 데이터 갯수 
+      let perPage = 5;
+      //블록당 보여줄 페이지 번호 갯수
+      let blockCount = 3;
+      //현재 페이지 블록 구하기 
+      let blockNum =Math.ceil(pageNumber / blockCount) ;
+      //블록 안에 있는 페이징의 시작번호 값 알아내기
+      let blockStart = ((blockNum - 1) * blockCount ) + 1 ;
+      //블록 안에 있는 페이징의 끝번호 값 알아내기
+      let blockEnd = blockStart + blockCount - 1 ;
+  
+      
+  
+      //데이터베이스 콜렉션에 있는 전체 객체의 갯수값 가져오는 명령어 
+      let totalData = await db.collection("board").countDocuments({});
+      //전체 데이터 값을 통해서 ---> 몇개의 페이징 번호가 만들어져야 하는지 계산
+      let paging =  Math.ceil(totalData / perPage) ;
+      //블록에서 마지막 번호가 페이징의 끝번호보다 크ㅏ면 페이징의 끝번호를 강제로 부여
+      if(blockEnd > paging){
+          blockEnd = paging;
+      }
+      //블록의 총 갯수 구하기
+      let totalBlock = Math.ceil(paging / blockCount);
+      //데이터베이스에서 꺼내오는 데이터의 시작 순번값을 결정 
+      let startFrom = (pageNumber -1 ) * perPage;
+
+
+    db.collection("board").find({}).sort({brdno:-1}).skip(startFrom).limit(perPage).toArray((err,result)=>{
+        res.render("brdlist",{
+            userdata:req.user,
+            brddata:result,
+            paging:paging,
+            pageNumber:pageNumber,
+            blockStart:blockStart,
+            blockEnd:blockEnd,
+            blockNum:blockNum,
+            totalBlock:totalBlock
+        })
     })
 });
 
@@ -126,7 +214,7 @@ app.get("/delete/:no",(req,res)=>{
 });
 
 
-//brd 입력 날짜 
+//brd 파일 첨부
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, 'public/upload')
@@ -158,6 +246,42 @@ app.post("/add/brd",(req,res)=>{
         });
     });
 });
+
+
+//all 메뉴 페이지 
+app.get("/shop/all",(req,res)=>{
+    db.collection("prdlist").find().toArray((err,result)=>{
+        res.render("shop",{prdData:result,userdata:req.user});
+    })
+});
+app.get("/shop/sofatable",(req,res)=>{
+    db.collection("prdlist").find({category:"sofa table"}).toArray((err,result)=>{
+        res.render("shop",{prdData:result,userdata:req.user});
+    })
+});
+app.get("/shop/chair",(req,res)=>{
+    db.collection("prdlist").find({category:"chair"}).toArray((err,result)=>{
+        res.render("shop",{prdData:result,userdata:req.user});
+    })
+});
+app.get("/shop/barchair",(req,res)=>{
+    db.collection("prdlist").find({category:"barchair"}).toArray((err,result)=>{
+        res.render("shop",{prdData:result,userdata:req.user});
+    })
+});
+app.get("/shop/sofa",(req,res)=>{
+    db.collection("prdlist").find({category:"sofa"}).toArray((err,result)=>{
+        res.render("shop",{prdData:result,userdata:req.user});
+    })
+});
+
+//prd 상세 페이지
+app.get("/shopdetail/:no",(req,res)=>{
+    db.collection("prdlist").findOne({num:Number(req.params.no)},(err,result)=>{
+        res.render("shopdetail",{prdData:result,userdata:req.user})
+    })
+});
+
 
 
 
@@ -251,103 +375,31 @@ passport.use(new LocalStrategy({
 //관리자 관련 
 
 
-// passport.use(new LocalStrategy({
-//     usernameField: 'adminId', // 태그 네임값
-//     passwordField: 'adminPass',
-//     session: true,
-//     passReqToCallback: false,
-//   }, function (id, pass, done) {
-//                                  //작명변수//태그 네임값
-//     db.collection('admin').findOne({ adminId: id }, function (err, result) {
-//       if (err) return done(err)
-  
-//       if (!result) return done(null, false, { message: '존재하지않는 아이디 입니다.' })
-//       if (pass == result.adminPass) {
-//         return done(null, result)
-//       } else {
-//         return done(null, false, { message: '비번이 틀렸습니다' })
-//       }
-//     })
-//   }));
-
-//     //처음 로그인 했을 시 해당 사용자의 아이디를 기반으로 세션을 생성함
-//   //  req.user
-//   passport.serializeUser(function (user, done) {
-//     done(null, user.adminId) //서버에다가 세션만들어줘 -> 사용자 웹브라우저에서는 쿠키를 만들어줘
-//   });
-  
-//   // 로그인을 한 후 다른 페이지들을 접근할 시 생성된 세션에 있는 회원정보 데이터를 보내주는 처리
-//   // 그전에 데이터베이스 있는 아이디와 세션에 있는 회원정보중에 아이디랑 매칭되는지 찾아주는 작업
-//   passport.deserializeUser(function (adminId, done) {
-//       db.collection('admin').findOne({adminId:adminId }, function (err,result) {
-//         done(null, result);
-//       })
-//   }); 
-
-
-
-  
-// 관리자 로그인 페이지 화면 
-app.get("/admin",(req,res)=>{
-    res.render("admin_login",{admindata:req.user}); //admin_login.ejs 파일로 응답
- 
-});
-
-
-//관리자 화면 로그인
-app.post("/login",passport.authenticate('local', {failureRedirect : '/fail'}),(req,res)=>{
-    res.redirect("admin/prdlist");
-    //로그인 성공시 관리자 상품등록 페이지로 이동 
-});
-
-//로그인 실패시 
-app.get("/fail",(req,res)=>{
-    res.send("로그인 실패")
-});
-
-//로그아웃
-app.get("/logout",function(req,res){
-    req.session.destroy(function(err){
-        res.clearCookie("connect.sid");
-        res.redirect("/");
-    })
-});
-
 //관리자 상품 등록 페이지 열기
 app.get("/admin/prdlist",function(req,res){
     db.collection("prdlist").find({}).toArray(function(err,result){
-        res.render("admin_prdlist",{prdData:result,admindata:req.user});
+        res.render("admin_prdlist",{prdData:result,userdata:req.user});
     })
 });
 
 //상품을 db 에 넣기 
 app.post("/add/prdlist",upload.array('thumbnail'),(req,res)=>{
+
+    let fileTest = [];
+
     if(req.files){
-        fileTest=req.files[0].originalname;
-        fileTest1=req.files[1].originalname;
-        fileTest2=req.files[2].originalname;
-        fileTest3=req.files[3].originalname;
-        fileTest4=req.files[4].originalname;
-        fileTest5=req.files[5].originalname;
+        for(let i=0; i<req.files.length; i++){
+            fileTest[i] = req.files[i].originalname;
+        }
     }
     else { 
-        fileTest=null;
-        fileTest1=null;
-        fileTest2=null;
-        fileTest3=null;
-        fileTest4=null;
-        fileTest5=null;
+        fileTest = null;
     }
     db.collection("count").findOne({name:"상품등록"},(err,result)=>{
         db.collection("prdlist").insertOne({
             num:result.prdCount+1,
             name:req.body.name,
             thumbnail:fileTest,
-            contImg1:fileTest1,
-            contImg2:fileTest2,
-            contImg3:fileTest3,
-            contImg4:fileTest4,
-            contImg5:fileTest5,
             category:req.body.category,
             price:req.body.price,
             address:req.body.address,
@@ -359,3 +411,154 @@ app.post("/add/prdlist",upload.array('thumbnail'),(req,res)=>{
     })
 })
 
+
+//관리자 매장 등록 페이지 열기 
+app.get("/admin/storelist",function(req,res){
+    db.collection("storelist").find({}).toArray(function(err,result){
+        res.render("admin_storelist",{storeData:result,userdata:req.user});
+    })
+});
+
+//매장등록페이지에서 전송한 값 데이터베이스에 삽입
+app.post("/addstore",upload.single('thumbnail'),(req,res)=>{
+    db.collection("count").findOne({name:"매장등록"},(err,result1)=>{
+     
+        db.collection("storelist").insertOne({
+            num:result1.storeCount + 1,
+            name:req.body.name,
+            sido:req.body.city1,
+            sigugun:req.body.city2,
+            address:req.body.detail,
+            phone:req.body.phone,
+            time:req.body.time,
+        },(err,result)=>{
+            db.collection("count").updateOne({name:"매장등록"},{$inc:{storeCount:1}},(err,result)=>{
+                res.redirect("/admin/storelist"); //매장등록 페이지로 이동
+            });
+        })
+    });
+});
+
+
+//매장 지역 검색 결과 페이지
+app.get("/search/storelocal",(req,res)=>{
+    //시/도는 선택했고 군구는 선택을 안했을 때
+    if(req.query.city1 !== "" && req.query.city2 === ""){
+        db.collection("storelist").find({sido:req.query.city1}).toArray((err,result)=>{
+            res.render("store",{storeData:result,userdata:req.user});
+        });
+    }
+    //시도 선택 및 군구 선택 했을 때
+    else if(req.query.city1 !== "" && req.query.city2 !== ""){
+        db.collection("storelist").find({sido:req.query.city1,sigugun:req.query.city2}).toArray((err,result)=>{
+            res.render("store",{storeData:result,userdata:req.user});
+        });
+    }
+    //둘다 선택 안했을 경우
+    else {
+        res.redirect("/store");
+    }
+});
+
+app.get("/search/storename",(req,res)=>{
+    //atlas사이트에서 제공하는 검색기능 코드
+    let search = [
+        {
+          $search: {
+            index: "store_search",
+            text: {
+              query: req.query.name, //store에서 입력한 매장명 데이터
+              path: "name"// storelist콜렉션에서 name 프로퍼티
+            }
+          }
+        }
+      ]
+
+    //aggregate() 사용해서 storelist 콜렉션에서 데이터 꺼내옴
+    //검색어 입력시
+    if(req.query.name !== "") {
+        db.collection("storelist").aggregate(search).toArray((err,result)=>{
+            res.render("store",{storeData:result,userdata:req.user});
+        })
+    }
+    //검색어 미입력시
+    else{
+        res.redirect("/store");
+    }
+})
+
+// 관리자 뉴스 등록 페이지 
+app.get("/admin/newslist",function(req,res){
+    db.collection("newslist").find({}).toArray(function(err,result){
+        res.render("admin_newslist",{newsData:result,userdata:req.user});
+    })
+});
+
+//뉴스 데이터 db 전송 
+//brd db 에 데이터 전송 
+app.post("/add/newslist",upload.array('thumbnail'),(req,res)=>{
+    let times = moment().format("YYYY.MM.DD");
+
+    let fileTest = [];
+    let address = [];
+
+    console.log(req.body.address)
+
+    // for(let j=0; j < req.body.address.length; j++){
+    //     address[j] = req.body.address[j]
+    // }
+
+    if(req.files){
+        for(let k=0; k<req.files.length; k++){
+            fileTest[k] = req.files[k].originalname;
+        }
+    }
+    else { 
+        fileTest = null;
+    }
+    db.collection("count").findOne({name:"뉴스등록"},(err,result)=>{
+        db.collection("newslist").insertOne({
+            num:result.newsCount + 1,
+            month:req.body.month,
+            title:req.body.title,
+            address:req.body.address,
+            thumbnail:fileTest,
+            date:times
+        },(err,result)=>{
+            db.collection("count").updateOne({name:"뉴스등록"},{$inc:{newsCount:1}},(err,result)=>{
+                res.redirect("/admin/newslist");
+            });
+        });
+    });
+});
+
+//관리자 공지리스트 페이지
+app.get("/admin/noticelist",function(req,res){
+    db.collection("noticelist").find({}).toArray(function(err,result){
+        res.render("admin_noticelist",{noticeData:result,userdata:req.user});
+    })
+});
+//공지 데이터베이스에 삽입
+app.post("/add/noticelist",(req,res)=>{
+    let times = moment().format("YYYY.MM.DD");
+
+    db.collection("count").findOne({name:"공지등록"},(err,result)=>{
+        db.collection("noticelist").insertOne({
+            num:result.noticeCount + 1,
+            title:req.body.title,
+            content:req.body.content,
+            date:times
+        },(err,result)=>{
+            db.collection("count").updateOne({name:"공지등록"},{$inc:{noticeCount:1}},(err,result)=>{
+                res.redirect("/admin/noticelist");
+            });
+        });
+    });
+});
+
+//공지 삭제
+app.get("/noticedelete/:no",(req,res)=>{
+    db.collection("notice").deleteOne({num:Number(req.params.no)},(err,result)=>{
+        res.redirect("/notice");
+    })
+});
